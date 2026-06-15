a90a08cc5eb394937eceaeac0135465605289008
Joana <jo_areas@hotmail.com>
2026-06-15 12:32:14 -0300

    refactor: update cart_item specs to use FactoryBot

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  let(:cart) { Cart.create!(total_price: 0) }
-  let(:product) { Product.create!(name: 'Produto Teste', price: 9.99) }
-
   context 'when validating quantity' do
     it 'validates presence of quantity' do
-      cart_item = described_class.new(cart:, product:)
+      cart_item = build(:cart_item, quantity: nil)
 
       expect(cart_item.valid?).to be_falsey
       expect(cart_item.errors[:quantity]).to include("can't be blank")
     end
 
     it 'validates numericality of quantity' do
-      cart_item = described_class.new(cart:, product:, quantity: 0)
+      cart_item = build(:cart_item, quantity: 0)
 
       expect(cart_item.valid?).to be_falsey
       expect(cart_item.errors[:quantity]).to include("must be greater than 0")
@@ -22,14 +19,14 @@ RSpec.describe CartItem, type: :model do
 
   context 'when validating associations' do 
     it 'is invalid without a cart' do
-      cart_item = described_class.new(product:, quantity: 1)
+      cart_item = build(:cart_item, cart: nil)
 
       expect(cart_item.valid?).to be_falsey
       expect(cart_item.errors[:cart]).to include("must exist")
     end
 
     it 'is invalid without a product' do
-      cart_item = described_class.new(cart:, quantity: 1)
+      cart_item = build(:cart_item, product: nil)
 
       expect(cart_item.valid?).to be_falsey
       expect(cart_item.errors[:product]).to include("must exist")
