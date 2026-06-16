2ac75efd9663a9042d3661359a4cf122bfc7a93b
Joana <jo_areas@hotmail.com>
2026-06-16 20:46:44 -0300

    feat: add POST /cart endpoint to add product to cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,4 +4,8 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+  
+  def update_total_price(product, quantity)
+    update!(total_price: total_price + product.price * quantity)
+  end
 end
