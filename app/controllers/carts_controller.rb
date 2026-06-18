9b8e7be25ae55a454b79221723f582e545a2f5ef
Joana <jo_areas@hotmail.com>
2026-06-18 15:14:43 -0300

    feat: add GET /cart endpoint to retrieve cart products

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,20 @@
 class CartsController < ApplicationController
+  before_action :set_cart
+
   def create
-    @cart = find_or_create_cart
     add_product_to_cart
     render json: CartSerializer.new(@cart).as_json
   end
 
+  def show
+    render json: CartSerializer.new(@cart).as_json
+  end
+
   private
+
+  def set_cart
+    @cart = find_or_create_cart
+  end
 
   def find_or_create_cart
     Cart.find_by(id: session[:cart_id]) || create_cart
@@ -17,6 +26,8 @@ class CartsController < ApplicationController
     end
   end
 
+  # TODO move logic to Cart model
+  # TODO add error handling for RecordNotFound
   def add_product_to_cart
     product_id, quantity = cart_item_params.values_at(:product_id, :quantity)
   
