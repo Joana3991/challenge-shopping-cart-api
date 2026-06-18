fcdc1c429ee1af5447f4bf8359b7e43819b417b9
Joana <jo_areas@hotmail.com>
2026-06-18 19:57:47 -0300

    refactor: move cart business logic to model and add error handling

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,8 +4,17 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
-  
-  def update_total_price(product, quantity)
+
+  def add_product_to_cart(product:, quantity:)
+    ActiveRecord::Base.transaction do
+      cart_items.create!(product:, quantity:)
+      update_total_price(product:, quantity:)
+    end
+  end
+
+  private
+
+  def update_total_price(product:, quantity:)
     update!(total_price: total_price + product.price * quantity)
   end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -9,21 +9,38 @@ RSpec.describe Cart, type: :model do
     end
   end
 
-  describe 'mark_as_abandoned' do
-    let(:shopping_cart) { create(:shopping_cart) }
+  # describe 'mark_as_abandoned' do
+  #   let(:shopping_cart) { create(:shopping_cart) }
 
-    it 'marks the shopping cart as abandoned if inactive for a certain time' do
-      shopping_cart.update(last_interaction_at: 3.hours.ago)
-      expect { shopping_cart.mark_as_abandoned }.to change { shopping_cart.abandoned? }.from(false).to(true)
+  #   it 'marks the shopping cart as abandoned if inactive for a certain time' do
+  #     shopping_cart.update(last_interaction_at: 3.hours.ago)
+  #     expect { shopping_cart.mark_as_abandoned }.to change { shopping_cart.abandoned? }.from(false).to(true)
+  #   end
+  # end
+
+  # describe 'remove_if_abandoned' do
+  #   let(:shopping_cart) { create(:shopping_cart, last_interaction_at: 7.days.ago) }
+
+  #   it 'removes the shopping cart if abandoned for a certain time' do
+  #     shopping_cart.mark_as_abandoned
+  #     expect { shopping_cart.remove_if_abandoned }.to change { Cart.count }.by(-1)
+  #   end
+  # end
+
+  describe '#add_product_to_cart' do
+    let!(:cart) { create(:cart, total_price: 10.0) }
+    let(:product) { create(:product, price: 15.0) }
+
+    it 'adds product to cart' do 
+      expect { cart.add_product_to_cart(product:, quantity: 1) }
+        .to change { cart.cart_items.count }.by(1)
+      expect(CartItem.last.product).to eq(product)  
     end
-  end
 
-  describe 'remove_if_abandoned' do
-    let(:shopping_cart) { create(:shopping_cart, last_interaction_at: 7.days.ago) }
+    it 'updates the cart total_price' do
+      cart.add_product_to_cart(product:, quantity: 2)
 
-    it 'removes the shopping cart if abandoned for a certain time' do
-      shopping_cart.mark_as_abandoned
-      expect { shopping_cart.remove_if_abandoned }.to change { Cart.count }.by(-1)
+      expect(cart.reload.total_price).to eq(40.0)
     end
   end
 end
