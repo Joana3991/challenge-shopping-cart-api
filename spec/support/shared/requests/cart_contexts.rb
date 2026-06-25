c2f1754dec5e2e7450613c7b9d3c825cad39acfc
Joana <jo_areas@hotmail.com>
2026-06-25 11:56:44 -0300

    fix: variable name on shared_contexts comment

--- spec/support/shared/requests/cart_contexts.rb
+++ spec/support/shared/requests/cart_contexts.rb
@@ -6,7 +6,7 @@ shared_context 'cart exists in session empty' do
   let(:cart) { Cart.find(session[:cart_id]) }
 end
 
-# Expects `existing_product` to be defined within example context
+# Expects `product` to be defined within example context
 shared_context 'cart exists in session with product' do |quantity: 2|
   before do
     post '/cart', params: { product_id: product.id, quantity: }, as: :json
